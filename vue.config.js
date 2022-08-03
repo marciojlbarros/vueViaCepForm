00c99998f4149c1f9f4b201ab4f8d8ad96c7ff85
marciojlbarros <marciojlbarros@gmail.com>
2022-08-03 11:47:08 -0300

    configurando proxy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'https://viacep.com.br/ws/',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   transpileDependencies: true
 })
